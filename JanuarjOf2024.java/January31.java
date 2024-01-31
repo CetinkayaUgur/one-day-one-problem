4a51b5daad30db7d74f3c74865628cc9c3c24f73
CetinkayaUgur <contactugurcetinkaya@gmail.com>
2024-01-31 20:01:50 +0300

    I solved Leetcode's 1st problem(two sum)

---
+++ JanuarjOf2024.java/January31.java
@@ -0,0 +1,21 @@
+class January31 {
+    /*
+     * https://leetcode.com/problems/two-sum/description/
+     */
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (j == i) {
+                    continue;
+                } else if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+}
