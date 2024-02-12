7b85be8f5a1e3bcf894c0f5c0e9c4fff028e8474
CetinkayaUgur <contactugurcetinkaya@gmail.com>
2024-02-12 23:36:44 +0300

    I solved one HackerRank problem(java datatypes)

---
+++ FebruaryOf2024/February12.java
@@ -0,0 +1,36 @@
+package FebruaryOf2024;
+
+import java.util.*;
+
+public class February12 {
+    /*
+     * https://www.hackerrank.com/challenges/java-datatypes/problem?isFullScreen=true
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        String result = "";
+
+        for (int i = 0; i < T; i++) {
+            try {
+                long x = scanner.nextLong(); 
+                System.out.println(x + " can be fitted in:");
+
+                if (x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE) {
+                    System.out.println("* byte");
+                }
+                if (x >= Short.MIN_VALUE && x <= Short.MAX_VALUE) {
+                    System.out.println("* short");
+                }
+                if (x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE) {
+                    System.out.println("* int");
+                }
+                if (x >= Long.MIN_VALUE && x <= Long.MAX_VALUE) {
+                    System.out.println("* long");
+                }
+            } catch (Exception e) {
+                System.out.println(scanner.next() + " can't be fitted anywhere.");
+            }
+        }
+    }
+}
