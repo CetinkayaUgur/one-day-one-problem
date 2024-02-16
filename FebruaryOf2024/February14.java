b283322622a04f0fae6eeca1485f4187f47df134
CetinkayaUgur <contactugurcetinkaya@gmail.com>
2024-02-16 22:13:34 +0300

    February14 Update

--- FebruaryOf2024/February14.java
+++ FebruaryOf2024/February14.java
@@ -4,15 +4,24 @@ public class February14 {
     /*
      * https://leetcode.com/problems/plus-one
      */
-    public int[] plusOne(int[] digits) {
+    public static void main(String[] args) {
+        int[] digs = {9,8,7,6,5,4,3,2,1,0}; 
+        int[] dig = plusOne(digs);
+        
+        for (int i = 0; i < dig.length; i++) {
+            System.out.print(dig[i]);
+        }
+    }
+    public static int[] plusOne(int[] digits) {
         int number = 0;
         
-        for (int i = 0; i < digits.length; i++) {
-            number += digits[i]*Math.pow(10, digits.length-1-i);
+        for (int digit : digits) {
+            number = number * 10 + digit;
         }
+        
+        number++; //add one to digits' number
 
-        number++; //add one to digits number
-
+        //We can find how many digits a number has this way "(int) Math.log10(number) + 1"
         int[] oneAddedArray = new int[(int) Math.log10(number) + 1];
         for (int i = oneAddedArray.length - 1; i >= 0; i--) {
             oneAddedArray[i] = number % 10;
