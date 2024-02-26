42067997187c52300ec285a7292b12751d704ef8
CetinkayaUgur <contactugurcetinkaya@gmail.com>
2024-02-26 13:32:10 +0300

    I solved one LeetCode problem(same tree)

---
+++ FebruaryOf2024/February20.java
@@ -0,0 +1,34 @@
+package FebruaryOf2024;
+
+public class February20 {
+    /*
+     * https://leetcode.com/problems/same-tree/?envType=daily-question&envId=2024-02-26
+     */
+
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // Both of tree are null then its true
+        if (p == null && q == null) return true;
+        
+        // if one of them is null but other one have a value or have different values then its false
+        if (p == null || q == null || p.val != q.val) return false;
+        
+        // Check every subtrees
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
