b343440cfa867c3988008e5920026533657aecc1
CetinkayaUgur <contactugurcetinkaya@gmail.com>
2024-03-01 23:05:34 +0300

    Create February26.java

---
+++ FebruaryOf2024/February26.java
@@ -0,0 +1,20 @@
+package FebruaryOf2024;
+
+public class February26 {
+    /*
+     * 
+     */
+    public boolean hasCycle(ListNode head) {
+        
+        return true;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
