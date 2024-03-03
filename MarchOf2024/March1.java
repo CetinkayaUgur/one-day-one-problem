e56e2015a5fb054f3e11fad66008d11f824f9a26
CetinkayaUgur <contactugurcetinkaya@gmail.com>
2024-03-03 23:49:25 +0300

    I solved one LeetCode problem(206)

--- MarchOf2024/March1.java
+++ MarchOf2024/March1.java
@@ -1,5 +1,18 @@
 package MarchOf2024;
 
 public class March1 {
-    
+    /*
+     * https://leetcode.com/problems/reverse-linked-list/description/
+     */
+    public ListNode reverseList(ListNode head) {
+        
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
