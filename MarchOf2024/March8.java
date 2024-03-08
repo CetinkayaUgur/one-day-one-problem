e539b55473740830224c45cdf0661176c1c0ea20
CetinkayaUgur <contactugurcetinkaya@gmail.com>
2024-03-08 23:22:54 +0300

    Updated March8.java

---
+++ MarchOf2024/March8.java
@@ -0,0 +1,8 @@
+package MarchOf2024;
+
+public class March8 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode intersectVal = ;
+        return intersectVal;
+    }
+}
