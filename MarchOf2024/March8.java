5fac3875719b61cf2b5705de700a705607c31da3
CetinkayaUgur <contactugurcetinkaya@gmail.com>
2024-03-09 22:19:45 +0300

    Updated March8.java

--- MarchOf2024/March8.java
+++ MarchOf2024/March8.java
@@ -2,7 +2,18 @@ package MarchOf2024;
 
 public class March8 {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode intersectVal = ;
+        ListNode intersectVal = new ListNode(0);
+        if(headA == null || headB == null) return intersectVal;
+        
         return intersectVal;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
     }
 }
