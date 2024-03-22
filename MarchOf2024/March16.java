0e69648327e74ab3b126e98406cba5adce078ed5
CetinkayaUgur <contactugurcetinkaya@gmail.com>
2024-03-22 19:56:41 +0300

    Updated files

--- MarchOf2024/March16.java
+++ MarchOf2024/March16.java
@@ -1,5 +1,10 @@
 package MarchOf2024;
 
+import java.util.Scanner;
+
 public class March16 {
-    
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+        System.out.println("Harf giriniz: ");
+    }
 }
