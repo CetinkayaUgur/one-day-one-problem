1e3acf9e341a32e8138ced02b19f19b1139fc7a7
CetinkayaUgur <contactugurcetinkaya@gmail.com>
2024-03-29 23:54:26 +0300

    I coded an factorial algorithm

--- MarchOf2024/March20.java
+++ MarchOf2024/March20.java
@@ -1,5 +1,12 @@
 package MarchOf2024;
 
 public class March20 {
-    
+    public static int fact(int number){
+        int total = 1;
+        if (number == 1) {
+            return total;
+        }
+        total *= number;
+        return fact(number-1);
+    }
 }
